e62761061a89f2a8cf4c66bf45ecf2f5d777e1fc
William Bowman <31966111+wsbowman@users.noreply.github.com>
2020-01-31 17:09:04 -0500

    Updated about
    Leftover github plugin stuff removed

--- SSH_Plugin/__init__.py
+++ SSH_Plugin/__init__.py
@@ -1,23 +1,7 @@
 
 """
     SSHplugin
-    This plugin provides an interface to the Github v3 API.
-    The Github Hook extends the HttpHook and accepts both Basic and
-    Token Authentication. If both are available, the hook will use
-    the specified token, which should be in the following format in
-    the extras field: {"token":"XXXXXXXXXXXXXXXXXXXXX"}
-        The host value in the Hook should contain the following:
-        https://api.github.com/
-    The Github Operator provides support for the following endpoints:
-        Comments
-        Commits
-        Commit Comments
-        Issue Comments
-        Issues
-        Members
-        Organizations
-        Pull Requests
-        Repositories
+    This plugin provides an interface to SSH.
 """
 
 from airflow.plugins_manager import AirflowPlugin
